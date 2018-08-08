7440754632cfdb8b5eb1a0df18861830406a2fed
Mikaela Sheerin <mikasheerin@gmail.com>
2018-08-08 16:41:29 -0400

    Initial styling

---
+++ form/default.inc.php
@@ -0,0 +1,20 @@
+<style type="text/css">
+  body {
+    background: url(../images/pool.jpg) no-repeat;
+    background-size: cover;
+    font: 18px 'Muli', sans-serif;
+    color: #3F94A8;
+  }
+
+  div {
+    background-color: white;
+    margin: 10px;
+    padding: 20px;
+    width: 50%;
+  }
+</style>
+
+<div>
+  <h1>Oops! Something went wrong with your submission.</h1>
+  <p>Please try again.</p>
+</div>
---
+++ form/thankyou.inc.php
@@ -0,0 +1,24 @@
+<style type="text/css">
+  body {
+    background: url(../images/pool.jpg) no-repeat;
+    background-size: cover;
+    font: 18px 'Muli', sans-serif;
+    color: #3F94A8;
+  }
+
+  div {
+    background-color: white;
+    margin: 50px auto;
+    padding: 20px;
+    width: 50%;
+    border-color: #3F94A8;
+    border-style: solid;
+  }
+</style>
+
+<body>
+  <div>
+    <h1>Thank you for your inquiry</h1>
+    <p>Thanks for your interest. We have received your message, and we will be in touch shortly.</p>
+  </div>
+</body>
