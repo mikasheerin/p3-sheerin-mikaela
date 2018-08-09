3e434dc9cd281f71d4a9206dc9ec38f93ea94725
Mikaela Sheerin <mikasheerin@gmail.com>
2018-08-09 01:07:24 -0400

    added firs two sets of videos & styles

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,17 @@ $(document).ready(function(){
   $('.about-slider ').slick({
     dots: true,
     autoplay: true,
-    
+  });
+});
+
+$(document).ready(function(){
+  $('.atlanta-slider ').slick({
+    dots: true,
+  });
+});
+
+$(document).ready(function(){
+  $('.babydriver-slider ').slick({
+    dots: true,
   });
 });
